c9b40bbdf46655c969113d2d1213e84317032dea
covoer <l1505570968@gmail.com>
2023-09-24 20:36:08 +1000

    Refactor result checking program to use functions

---
+++ prac_02/score.py
@@ -0,0 +1,14 @@
+def main():
+    score = float(input("Enter score: "))
+    result = get_result(score)
+    print(result)
+def get_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score > 90:
+        return "Excellent"
+    elif score > 50:
+        return "Passable"
+    else:
+        return "Bad"
+main()
