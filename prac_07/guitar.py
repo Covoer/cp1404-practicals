c7853cecacaba50d3a2c23b3c98325c0fca1abf1
covoer <l1505570968@gmail.com>
2023-11-05 19:58:50 +1000

    Complete second part of More Guitars exercise

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -1,8 +1,3 @@
-import csv
-
-filename = 'guitars.csv'
-
-
 class Guitar:
     def __init__(self, name="", year=0, cost=0.0):
         self.name = name
@@ -24,37 +19,56 @@ class Guitar:
         return self.get_age() >= 50
 
 
-def load_guitars(filename):
+def read_guitars(filename):
     guitars = []
     with open(filename, 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            if row:  # skip empty rows
-                name, year, cost = row
-                guitars.append(Guitar(name, int(year), float(cost)))
+        for line in file:
+            parts = line.strip().split(',')
+            if len(parts) == 3:  # Ensure there are enough parts
+                name, year, cost = parts
+                guitars.append(Guitar(name, int(year), float(cost)))  # Convert year and cost to the correct types
     return guitars
 
 
-def print_guitars(guitars):
+def save_guitars(filename, guitars):
+    with open(filename, 'w', newline='') as file:
+        for guitar in guitars:
+            file.write("{},{},{}\n".format(guitar.name, guitar.year, guitar.cost))
+
+
+def main():
+    filename = 'guitars.csv'
+    guitars = read_guitars(filename)
+
+    # Display the unsorted list of guitars
+    print("These are my guitars:")
+    for guitar in guitars:
+        print(guitar)
+
+    # Sort and display the guitars by year
+    guitars.sort()
+    print("\nGuitars sorted by year:")
+    for guitar in guitars:
+        print(guitar)
+
+    # Add new guitars
+    add_more = input("Do you want to add more guitars? (Y/n) ").lower()
+    while add_more != 'n':
+        name = input("Name: ")
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitars.append(Guitar(name, year, cost))
+        add_more = input("Add another? (Y/n) ").lower()
+
+    # Save guitars to file
+    save_guitars(filename, guitars)
+
+    # Verify by reading the file again
+    print("\nGuitars saved to file:")
+    guitars = read_guitars(filename)
     for guitar in guitars:
         print(guitar)
 
 
-# Load guitars from file
-guitars = load_guitars(filename)
-
-# Display unsorted guitars
-print("Unsorted Guitars:")
-print_guitars(guitars)
-
-# Sort guitars by year (oldest to newest)
-guitars.sort()
-
-# Display sorted guitars
-print("\nSorted Guitars:")
-print_guitars(guitars)
-# Example usage:
-guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
-print(guitar)  # Output: Gibson L-5 CES (1922) : $16,035.40
-print(guitar.get_age())  # Output: 100
-print(guitar.is_vintage())  # Output: True
+if __name__ == "__main__":
+    main()
