09d7bbf7a2021806f8219207e5729f8789f3a171
Nathaniel-Carl-Peter <144785279+Nathaniel-Carl-Peter@users.noreply.github.com>
2023-11-14 23:57:09 +1000

    Update box_lauput_demo.py

--- prac_08/box_lauput_demo.py
+++ prac_08/box_lauput_demo.py
@@ -5,7 +5,7 @@ from kivy.lang import Builder
 class BoxLayoutDemo(App):
     def build(self):
         self.title = "Box Layout Demo"
-        self.root = Builder.load_file('box_layout.kv') #  file name wrong spelling
+        self.root = Builder.load_file('box_layout.kv') #  file name wrong spelling "box_la"
         return self.root
 
     def handle_greet(self):
